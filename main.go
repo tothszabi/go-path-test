d858f32bdee7cb86c17b80bd2895c6db17bf4f5b
Szabolcs Toth <szabolcs.toth@bitrise.io>
2023-05-15 13:57:06 +0100

    wip

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 
 func main() {
 	paths := []string{
-		"",
-		"",
+		"./",
+		"./scripts/ci/bitrise/setup_bitrise_steps",
 	}
 
 	fmt.Println("Running path expansion with the GO runtime")
@@ -17,24 +17,23 @@ func main() {
 	for _, path := range paths {
 		expandedPath, err := filepath.Abs(os.ExpandEnv(path))
 		if err != nil {
-			fmt.Printf("%s encoundtered an error: %s", path, err)
+			fmt.Printf("%s encoundtered an error: %s\n", path, err)
 			continue
 		}
 
-		fmt.Printf("%s -> %s", path, expandedPath)
+		fmt.Printf("%s -> %s\n", path, expandedPath)
 	}
 
-	fmt.Println("")
-	fmt.Println("Running path expansion with custom stuff")
+	fmt.Printf("\nRunning path expansion with custom stuff\n")
 
 	for _, path := range paths {
 		currentDir, err := os.Getwd()
 		if err != nil {
-			fmt.Printf("current dir error: %s", err)
+			fmt.Printf("current dir error: %s\n", err)
 			continue
 		}
 
 		expandedPath := filepath.Join(currentDir, path)
-		fmt.Printf("%s -> %s", path, expandedPath)
+		fmt.Printf("%s -> %s\n", path, expandedPath)
 	}
 }
